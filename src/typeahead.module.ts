f1e298f9783ea98f2263dfd17c8ba8bcf029c282
Adrian Księżarczyk <adrian.ksiezarczyk@gorrion.pl>
2017-07-13 15:05:51 +0200

    replaced browser module to common module

--- src/typeahead.module.ts
+++ src/typeahead.module.ts
@@ -1,16 +1,16 @@
-import { NgModule, ModuleWithProviders }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule }          from '@angular/forms';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PropertyHandler } from './PropertyHandler';
 import { AutoComplete } from './autocomplete';
-import { HtmlOutlet} from '../src/html-outlet';
+import { HtmlOutlet } from '../src/html-outlet';
 
 @NgModule({
-  imports:      [ BrowserModule,  FormsModule, ReactiveFormsModule],
-  declarations: [ AutoComplete, HtmlOutlet],
-  exports: [ AutoComplete, HtmlOutlet],
-  providers:[PropertyHandler]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  declarations: [AutoComplete, HtmlOutlet],
+  exports: [AutoComplete, HtmlOutlet],
+  providers: [PropertyHandler]
 })
 export class TypeAheadModule {
-  static forRoot(): ModuleWithProviders { return {ngModule: TypeAheadModule, providers: [PropertyHandler]}; }
+  static forRoot(): ModuleWithProviders { return { ngModule: TypeAheadModule, providers: [PropertyHandler] }; }
 }
